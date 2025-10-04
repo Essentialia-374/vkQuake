3af4632abf7f1a17bece7a16cbda23431de6b29c
Essentialia <69067479+Essentialia-374@users.noreply.github.com>
2025-10-05 04:57:04 +0800

    Update gl_raytrace.h

--- Quake/gl_raytrace.h
+++ Quake/gl_raytrace.h
@@ -5,24 +5,38 @@
 
 typedef struct gl_raytrace_constants_s
 {
-        float screen_size_rcp_x;
-        float screen_size_rcp_y;
-        float aspect_ratio;
-        float origin_x;
-        float origin_y;
-        float origin_z;
-        float forward_x;
-        float forward_y;
-        float forward_z;
-        float right_x;
-        float right_y;
-        float right_z;
-        float down_x;
-        float down_y;
-        float down_z;
+	float screen_size_rcp_x;
+	float screen_size_rcp_y;
+	float aspect_ratio;
+
+	float origin_x;
+	float origin_y;
+	float origin_z;
+	float forward_x;
+	float forward_y;
+	float forward_z;
+	float right_x;
+	float right_y;
+	float right_z;
+	float down_x;
+	float down_y;
+	float down_z;
+
+	// NEW:
+	float	 aperture;		 // world units (0 = pinhole)
+	float	 focus_distance; // meters
+	float	 exposure;		 // multiplier for tonemapper
+	uint32_t frame_index;	 // progressive sample index (1..N)
 } gl_raytrace_constants_t;
 
-void GL_Raytrace_UpdateDescriptorSet (const VkDescriptorImageInfo *output_image_info, VkAccelerationStructureKHR tlas);
+// NEW signature: add accumulation & environment, optional material/light buffers
+void GL_Raytrace_UpdateDescriptorSet (
+	const VkDescriptorImageInfo *output_image_info, VkAccelerationStructureKHR tlas, const VkDescriptorImageInfo *accum_image_info,
+	const VkDescriptorImageInfo	 *env_tex_info,		   // combined image sampler
+	const VkDescriptorBufferInfo *materials_ssbo_info, // may be NULL
+	const VkDescriptorBufferInfo *lights_ssbo_info	   // may be NULL
+);
+
 void GL_Raytrace_Render (cb_context_t *cbx, int width, int height, const gl_raytrace_constants_t *constants);
 
 #endif /* GL_RAYTRACE_H */
